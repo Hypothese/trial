746f0c34b6d70f31b4811961f88013804caa0b07
Shvets <Nataliia.Shvets@campus.lmu.de>
2018-06-27 15:17:32 +0200

    This is print with a function

--- printme.R
+++ printme.R
@@ -1,7 +1,8 @@
 name = "Natalia"
-
 eyecolor = "brown"
-
 meal = "pork with mushroom sauce"
 
-print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+printer <- function() {
+  print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+}
+printer()
